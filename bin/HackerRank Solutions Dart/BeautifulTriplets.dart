63890b7cef50e5451b1da96df458ac8778508a87
Uday Garg <u.garg14@gmail.com>
2020-10-08 18:37:37 +0530

    beautiful triplets, efficient not mine

--- bin/HackerRank Solutions Dart/BeautifulTriplets.dart
+++ bin/HackerRank Solutions Dart/BeautifulTriplets.dart
@@ -1,20 +1,13 @@
 void main() {
-  var numbers = [1, 2, 4, 5, 7, 8, 10];
-  var d = 3;
+  var numbers = [2, 2, 3, 4, 5];
+  var d = 1;
   var count = 0;
 
   for (var i = 0; i < numbers.length; i++) {
-    for (var j = 1; j < numbers.length; j++) {
-      for (var k = 2; k < numbers.length; k++) {
-        var result1 = numbers[j] - numbers[i];
-
-        var result2 = numbers[k] - numbers[j];
-        if (result1 == d && result2 == d && result1 == result2) {
-          print('$i,$j,$k');
-          count++;
-        }
-      }
+    if (numbers.contains(numbers[i] + d) &&
+        numbers.contains(numbers[i] + (2 * d))) {
+      count++;
     }
   }
-  print(count++);
+  print(count);
 }
